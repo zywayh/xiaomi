b8936ced641524b8550cba4ec6c92667bf7dcffb
朱亚伟 <865308221@qq.com>
2018-03-15 15:33:14 +0800

    no message

---
+++ grails-app/controllers/xiaomi/AiController.groovy
@@ -0,0 +1,52 @@
+package xiaomi
+
+class AiController {
+
+    static responseFormats = ['json']
+
+    def index() {
+
+        println ("params:" + params)
+
+        try {
+            println ("json: " + request.JSON)
+        } catch(Exception e) {}
+
+        def root = [
+                version:"1.0",
+                session_attributes:[],     //持久化的内容可以放这
+                response:[
+                        open_mic:false,         //指示客户端是否需要关闭mic, true，打开麦克风；false，关闭麦克风
+                        to_speak:[              //和directive 二选一，复杂的用directive，简单的用tospeak
+                                type:0,
+                                text:"即将播放海浪的声音"
+                        ],
+                        to_display:[
+                                type:0,            // 0: TTS, 1: Audio, 2: ssml
+                                text:"即将播放海浪的声音"
+                        ],
+                        directives:[                //例如播放音频, 和tospeak 二选一，复杂的用directive，简单的用tospeak
+                                [
+                                        type:"audio",          // audio, tts
+                                        audio_item:[
+                                                stream:[
+                                                        url:"http://xxxxx.mp3",
+                                                        offset_in_milliseconds:0
+                                                ]
+                                        ]
+                                ],
+                                [
+                                        type:"tts",          // audio, tts
+                                        tts_item:[
+                                                "type": "text",
+                                                text:"http://xxxxx.mp3"
+                                        ]
+                                ]
+                        ],
+                        is_session_end:false
+                ]
+        ]
+
+        respond root
+    }
+}
---
+++ src/test/groovy/xiaomi/AiControllerSpec.groovy
@@ -0,0 +1,18 @@
+package xiaomi
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class AiControllerSpec extends Specification implements ControllerUnitTest<AiController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
