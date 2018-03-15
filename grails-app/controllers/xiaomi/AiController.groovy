329acb43f71f3daa0ed7864a27ddbe58c9500f6f
朱亚伟 <865308221@qq.com>
2018-03-15 16:14:04 +0800

    no message

--- grails-app/controllers/xiaomi/AiController.groovy
+++ grails-app/controllers/xiaomi/AiController.groovy
@@ -1,5 +1,8 @@
 package xiaomi
 
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
 class AiController {
 
     static responseFormats = ['json']
@@ -9,7 +12,46 @@ class AiController {
         println ("params:" + params)
 
         try {
-            println ("json: " + request.JSON)
+            println (request.JSON)
+            def d = [
+                request:[
+                        event_property:[:],
+                        type:0,
+                        locale:"zh-CN",
+                        request_id:"5064037ee9964fc4812ce0ded949772a",
+                        intent:[
+                                score:0.800000011920929,
+                                skillType:"Custom",
+                                request_type:"Start",
+                                query:"打开朱窝",
+                                domain:"openplatform",
+                                confidence:1,
+                                sub_domain:1005230,
+                                is_direct_wakeup:false,
+                                complete:true,
+                                app_id:291309754003751936,
+                                need_fetch_token:false
+                        ],
+                        timestamp:1521100053458
+                    ],
+                session:[
+                    application:[
+                        app_id:291309754003751936
+                    ],
+                    is_new:false,
+                    session_id:"291309754003751936_a5a8fd61-e8c7-4425-bc58-a6b0f8a34b1b",
+                    user:[
+                        access_token:"",
+                        user_id:"rplXtvcvirnpbI0ed6TTaA=="
+                    ]
+                ],
+                query:"打开朱窝",
+                context:[
+                    device_id:"OWLXq19m5LBJhaY9JrLFIQ=="
+                ],
+                version:0.1
+            ]
+
         } catch(Exception e) {}
 
         def root = [
@@ -34,19 +76,56 @@ class AiController {
                                                         offset_in_milliseconds:0
                                                 ]
                                         ]
-                                ],
-                                [
-                                        type:"tts",          // audio, tts
-                                        tts_item:[
-                                                "type": "text",
-                                                text:"海浪的声音"
-                                        ]
                                 ]
+//                                ,[
+//                                        type:"tts",          // audio, tts
+//                                        tts_item:[
+//                                                "type": "text",
+//                                                text:"海浪的声音"
+//                                        ]
+//                                ]
                         ],
                         is_session_end:false
                 ]
         ]
 
-        respond root
+        response.outputStream.write(new JsonBuilder(root).toString().getBytes("utf-8"))
+
+//        respond root
+    }
+
+
+    private static JsonSlurper jsonSlurper = new JsonSlurper()
+    private static JsonSlurper getJsonSlurper(){
+        if(!jsonSlurper)
+            jsonSlurper = new JsonSlurper()
+        return jsonSlurper
+    }
+
+    /**
+     * json字符串转json对象
+     * @param strJson
+     * @return
+     */
+    static def strJsonToJson(String strJson){
+        return getJsonSlurper().parseText(strJson)
+    }
+
+    /**
+     * 对象转json对象
+     * @param obj
+     * @return
+     */
+    static def objToJson(def obj){
+        return new JsonBuilder(obj)
+    }
+
+    /**
+     * 对象转json字符串
+     * @param obj
+     * @return
+     */
+    static def objToJsonStr(def obj){
+        return objToJson(obj).toString()
     }
 }
