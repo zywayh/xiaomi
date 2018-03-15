70d9865eff34f967624a829eb49719caf1089249
朱亚伟 <865308221@qq.com>
2018-03-15 16:42:49 +0800

    no message

--- grails-app/controllers/xiaomi/AiController.groovy
+++ grails-app/controllers/xiaomi/AiController.groovy
@@ -55,34 +55,43 @@ class AiController {
 
         def root = [
                 version:"1.0",
+                session:[
+                        attributes:"attributes"
+                ],
                 session_attributes:[],     //持久化的内容可以放这
+                is_session_end:false,
                 response:[
-                        open_mic:false,         //指示客户端是否需要关闭mic, true，打开麦克风；false，关闭麦克风
+                        open_mic:true,         //指示客户端是否需要关闭mic, true，打开麦克风；false，关闭麦克风
                         to_speak:[              //和directive 二选一，复杂的用directive，简单的用tospeak
                                 type:0,
                                 text:"即将播放海浪的声音"
                         ],
                         to_display:[
                                 type:0,            // 0: TTS, 1: Audio, 2: ssml
-                                text:"即将播放海浪的声音"
+                                text:"即将播放海浪的声音",
+                                ui_template:[
+                                        type:0
+                                ],
+                                widget:[
+                                        info:[]
+                                ]
                         ],
                         directives:[                //例如播放音频, 和tospeak 二选一，复杂的用directive，简单的用tospeak
                                 [
                                         type:"audio",          // audio, tts
                                         audio_item:[
                                                 stream:[
-                                                        url:"http://zyw-video.oss-cn-beijing.aliyuncs.com/6-3%E5%BE%90%E9%9D%99%E8%95%BE.mp3",
-                                                        offset_in_milliseconds:0
+                                                        url:"http://zyw-video.oss-cn-beijing.aliyuncs.com/6-3%E5%BE%90%E9%9D%99%E8%95%BE.mp3"
                                                 ]
                                         ]
+                                ],
+                                [
+                                        type:"tts",          // audio, tts
+                                        tts_item:[
+                                                "type": "text",
+                                                text:"海浪的声音"
+                                        ]
                                 ]
-//                                ,[
-//                                        type:"tts",          // audio, tts
-//                                        tts_item:[
-//                                                "type": "text",
-//                                                text:"海浪的声音"
-//                                        ]
-//                                ]
                         ],
                         is_session_end:false
                 ]
